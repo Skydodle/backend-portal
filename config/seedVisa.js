7a1e0310c92933447c173ca3824846f71c4602ec
I-Hsuan Hu <ihsuanhu@gmail.com>
2024-08-17 12:52:10 -0400

    seperate S3 test and visa seed

---
+++ config/seedVisa.js
@@ -0,0 +1,23 @@
+const db = require('./connection');
+const visaDocumentS = require('../models/VisaDocuments')
+
+
+// for visa documents seed
+const seedVisa = async () => {
+    try {
+      await visaDocumentS.create({
+        userid:'xxxxxx',
+        optReceipt:{name:'receipt'},
+        optEAD:{name:'ead'},
+        i983:{name:'i983'},
+        i20:{name:'i20'}
+      })
+  
+  
+    } catch (e) {
+      console.error('Error seeding visa documents dataset', e)
+    } finally {
+      await db.close()
+    }
+  }
+seedVisa()
