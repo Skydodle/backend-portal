9cd33917ffac032450546f2e7d9304e9588a96e0
Johnny Wu <skydodle@gmail.com>
2024-08-17 13:37:07 -0700

    Fix wrong controller file name

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,7 +9,7 @@ const {
   postRegisterUser,
   validateRegistrationToken,
 } = require('../controllers/authController');
-const { postLoginEmployee } = require('../controllers/loginController');
+const { postLoginUser } = require('../controllers/loginController');
 
 const authRouter = Router();
 
@@ -17,7 +17,7 @@ const authRouter = Router();
 authRouter.post('/register', validateNewUser, postRegisterUser);
 
 // Route to handle employee/HR login
-authRouter.post('/login', validateLoginUser, postLoginEmployee);
+authRouter.post('/login', validateLoginUser, postLoginUser);
 
 // Route to validate registration token before displaying the registration form
 authRouter.get('/validate-token', validateRegistrationToken);
