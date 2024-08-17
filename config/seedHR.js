e0c33de98a3561d90d676749692bcd773e842702
I-Hsuan Hu <111704465+IHsuanHu@users.noreply.github.com>
2024-08-17 13:02:44 -0400

    Portal 29 demo for push and get from s3 (#9)
    * Get Put Delete operations with S3
    
    * seperate S3 test and visa seed
    
    ---------
    
    Co-authored-by: Johnny Wu <skydodle@gmail.com>

--- config/seedHR.js
+++ config/seedHR.js
@@ -1,12 +1,9 @@
 const db = require('./connection');
 const argon2 = require('argon2');
 const User = require('../models/User');
-const visaDocumentS = require('../models/VisaDocuments');
 
 const seedHR = async () => {
   try {
-    await Employee.deleteMany({ role: 'HR' }); // Delete existing HR accounts
-    // await Employee.deleteMany(); 
     await User.deleteMany({ role: 'HR' }); // Delete existing HR accounts
 
     const hashedPassword = await argon2.hash('1234HR!');
@@ -17,11 +14,7 @@ const seedHR = async () => {
       password: hashedPassword,
       role: 'HR',
     });
-    // await Employee.create({
-    //   username: 'employeeTest123',
-    //   email: 'employee@company.com',
-    //   password: hashedPassword,
-    // });
+    
     console.log('HR account seeded successfully');
   } catch (error) {
     console.error('Error seeding HR account', error);
@@ -32,20 +25,3 @@ const seedHR = async () => {
 
 seedHR();
 
-// for visa documents seed
-const seedVisa = async () => {
-  try {
-    await visaDocumentS.create({
-      userid: 'xxxxxx',
-      optReceipt: { name: 'receipt' },
-      optEAD: { name: 'ead' },
-      i983: { name: 'i983' },
-      i20: { name: 'i20' },
-    });
-  } catch (e) {
-    console.error('Error seeding visa documents dataset', e);
-  } finally {
-    await db.close();
-  }
-};
-seedVisa();
