20a8939d3e23aaff56c23c8491a707a95e30305d
I-Hsuan Hu <ihsuanhu@gmail.com>
2024-08-18 00:13:28 -0400

    update visa schema

--- models/VisaDocuments.js
+++ models/VisaDocuments.js
@@ -4,21 +4,25 @@ const { Schema } = mongoose;
 const visaDocumentSchema = new Schema({
     userid: {type: String, require: true, unique: true,},
     optReceipt: { 
-        name: { type: String },
-        approved: { type: Boolean, default: false }
-      },
-      optEAD: { 
-        name: { type: String },
-        approved: { type: Boolean, default: false }
-      },
-      i983: { 
-        name: { type: String },
-        approved: { type: Boolean, default: false }
-      },
-      i20: { 
-        name: { type: String },
-        approved: { type: Boolean, default: false }
-      }
+      name: { type: String },
+      status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+      feedback: { type: String } // Optional: To store HR feedback if rejected
+  },
+  optEAD: { 
+      name: { type: String },
+      status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+      feedback: { type: String } // Optional: To store HR feedback if rejected
+  },
+  i983: { 
+      name: { type: String },
+      status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+      feedback: { type: String } // Optional: To store HR feedback if rejected
+  },
+  i20: { 
+      name: { type: String },
+      status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+      feedback: { type: String } // Optional: To store HR feedback if rejected
+  }
 
 });
 
