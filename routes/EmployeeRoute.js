0858c4c4ac8fcb643ed049b52701e4f6766c8c13
Johnny Wu <skydodle@gmail.com>
2024-08-21 16:44:00 -0700

    Fix import cap

--- routes/EmployeeRoute.js
+++ routes/EmployeeRoute.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const employeeRouter = express.Router();
-const { getOnboardingStatus, getUserProfile, postUserProfile, getAllEmployees, approveApplication, rejectApplication } = require('../controllers/onboardingController');
-const {validateJWT,hrAuthMiddleware} = require('../middlewares/authMiddleware');
+const {
+  getOnboardingStatus,
+  getUserProfile,
+  postUserProfile,
+  getAllEmployees,
+  approveApplication,
+  rejectApplication,
+} = require('../controllers/onboardingController');
+const {
+  validateJWT,
+  hrAuthMiddleware,
+} = require('../middlewares/AuthMiddleware');
 
 employeeRouter.get('/status', validateJWT, getOnboardingStatus);
 employeeRouter.get('/profile', validateJWT, getUserProfile);
 employeeRouter.post('/profile', validateJWT, postUserProfile);
 
-employeeRouter.get('/employees',validateJWT, hrAuthMiddleware,getAllEmployees);
-employeeRouter.put('/applications/approve/:id', validateJWT, hrAuthMiddleware, approveApplication);
-employeeRouter.put('/applications/reject/:id', validateJWT, hrAuthMiddleware, rejectApplication);
+employeeRouter.get(
+  '/employees',
+  validateJWT,
+  hrAuthMiddleware,
+  getAllEmployees,
+);
+employeeRouter.put(
+  '/applications/approve/:id',
+  validateJWT,
+  hrAuthMiddleware,
+  approveApplication,
+);
+employeeRouter.put(
+  '/applications/reject/:id',
+  validateJWT,
+  hrAuthMiddleware,
+  rejectApplication,
+);
 
 module.exports = employeeRouter;
