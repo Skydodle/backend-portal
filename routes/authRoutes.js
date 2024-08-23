ae51e0306b8a9959b8c2f11d154e61360c330f44
Johnny Wu <skydodle@gmail.com>
2024-08-22 19:18:11 -0700

    Add HR login endpoint

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,15 +9,21 @@ const {
   postRegisterUser,
   validateRegistrationToken,
 } = require('../controllers/authController');
-const { postLoginUser } = require('../controllers/loginController');
+const {
+  postLoginUser,
+  postLoginHR,
+} = require('../controllers/loginController');
 
 const authRouter = Router();
 
 // Route to handle employee/HR registration
 authRouter.post('/register', validateNewUser, postRegisterUser);
 
-// Route to handle employee/HR login
+// Route to handle employee/HR login to React app
 authRouter.post('/login', validateLoginUser, postLoginUser);
+
+// Route to handle only HR login to Angular app
+authRouter.post('/HRlogin', validateLoginUser, postLoginHR);
 
 // Route to validate registration token before displaying the registration form
 authRouter.get('/validate-token', validateRegistrationToken);
