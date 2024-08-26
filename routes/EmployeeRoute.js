36b1b4efc18725fd66fa7bd78b8c0c040f260b13
I-Hsuan Hu <ihsuanhu@gmail.com>
2024-08-26 10:43:10 -0400

    api for empolyee profile

--- routes/EmployeeRoute.js
+++ routes/EmployeeRoute.js
@@ -16,6 +16,10 @@ const {
   updatePersonalDetails,
   updateProfileImage,
 } = require('../controllers/employeeProfileController');
+const { getEmployeeInfoById, putEmployeeEmailById, putEmployeeInfoById, updateLicenseCopy, updateProfilePicture, updateVisaDocument } = require('../controllers/personalInfoController');
+const multer = require('multer');
+const upload = multer({dest: 'upload/'})
+
 employeeRouter.get('/status', validateJWT, getOnboardingStatus);
 employeeRouter.get('/profile', validateJWT, getUserProfile);
 employeeRouter.post('/profile', validateJWT, postUserProfile);
@@ -36,5 +40,12 @@ employeeRouter.put('/profile/car', updateCarInformation);
 employeeRouter.put('/profile/contact', updateContactInformation);
 employeeRouter.put('/profile/personal', updatePersonalDetails);
 employeeRouter.put('/profile/avatar', updateProfileImage);
+
+employeeRouter.get('/info', validateJWT, getEmployeeInfoById)
+employeeRouter.put('/info', validateJWT, putEmployeeInfoById)
+employeeRouter.put('/info/email', validateJWT, putEmployeeEmailById)
+employeeRouter.put('/info/profile', validateJWT, upload.single('file'), updateProfilePicture)
+employeeRouter.put('/info/visa', validateJWT, upload.single('file'), updateVisaDocument)
+employeeRouter.put('/info/license', validateJWT, upload.single('file'), updateLicenseCopy)
 
 module.exports = employeeRouter;
